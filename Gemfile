d3b41398a60ef16dbc332db68abc392c94cdd6bf
Kristian Mandrup <kmandrup@gmail.com>
2010-12-07 11:46:25 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
+gem "mongoid", ">= 2.0.0.beta.19"
+gem "bson_ext"
+
+group :development, :test do
+  gem "cucumber-rails"
+  gem "capybara"
+end
+
+gem "devise"
+gem "cream"
+gem "slim"
+gem "slim-rails"
+gem "haml", ">= 3.0.0"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiramizoo::Application.config.secret_token = '19a16b3c8b2a47de23ef7aaf793f4bdccca548a94e339b0403475f42e98e0cd3578b5bc6cb2563072670ca4687afab9733dc6e292dbc35652c940b1b1eba9154'
