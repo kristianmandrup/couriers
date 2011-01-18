129658d7f361c03e55ed16dd674bf149334784fa
Kristian Mandrup <kmandrup@gmail.com>
2011-01-18 15:32:03 +0100

    removed cache and reconfigured compass

--- config/compass.rb
+++ config/compass.rb
@@ -6,11 +6,8 @@ project_path = Compass::AppIntegration::Rails.root
 # Set this to the root of your project when deployed:
 http_path = "/"
 
-# css_dir = "tmp/stylesheets"
-# css_dir = "public/stylesheets/compiled"
-css_dir = "tmp/hassle/stylesheets/compiled"
-
-sass_dir = "app/stylesheets"
+css_dir   = "public/stylesheets/compiled"
+sass_dir  = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
