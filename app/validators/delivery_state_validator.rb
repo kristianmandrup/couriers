2afdac3a8d8369b1a8b1fcff0ca141a95fda215f
Kristian Mandrup <kmandrup@gmail.com>
2011-01-27 19:36:32 +0100

    fixed delivery state validator

--- app/validators/delivery_state_validator.rb
+++ app/validators/delivery_state_validator.rb
@@ -2,7 +2,7 @@ class DeliveryStateValidator < ActiveModel::EachValidator
 
   # ready|accepted|cancelled|arrived_at_pickup|arrived_at_dropoff|billed
   def delivery_states
-    Order::Delivery.delivery_states
+    Delivery.delivery_states
   end
 
   def validate_each(record, attribute, value)
