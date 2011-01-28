ac64fc8f9b118fdfb73b0ef44cd54b8f0e0e53b2
Kristian Mandrup <kmandrup@gmail.com>
2011-01-28 15:48:00 +0100

    delivery offer api fix

--- app/controllers/api/delivery_offers_controller.rb
+++ app/controllers/api/delivery_offers_controller.rb
@@ -30,8 +30,8 @@ module Api
     def answer
       begin
         # delivery_offer = Delivery::Offer.where(:number => delivery_id)
-        # delivery_offer.set_state state # may raise "business" error (lock)
         delivery_offer = Delivery::Offer.create_from :munich
+        delivery_offer.state = p_state
 
         reply_update delivery_offer, :state
       rescue DeliveryTimeOutError
